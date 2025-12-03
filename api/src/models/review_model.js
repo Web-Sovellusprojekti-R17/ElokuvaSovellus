a640556c5eef38a049bedc93e6ad3f4fcdc15955
Skyde789 <82514616+Skyde789@users.noreply.github.com>
2025-12-04 01:46:15 +0200

    Huge review update
    Reviews are now a component.
    Reviews now correctly require authentication to delete/edit/post.
    Reviews now correctly get the name of the user who wrote it.
    Reviews now have the correct userID instead of "1"
    You can no longer edit other peoples reviews
    Added more backend calls for reviews.
    
    Reviews page now shows your reviews if there is no movieID in the URL (arvostelu/:id) and shows all of the movie reviews if there is a movieID

--- api/src/models/review_model.js
+++ api/src/models/review_model.js
@@ -2,14 +2,43 @@ import pool from "../database.js";
 
 
 export async function getAll() {
-  const result = await pool.query("SELECT * FROM reviews");
+  const result = await pool.query( 
+    `SELECT reviews.*, users.username
+     FROM reviews
+     JOIN users ON reviews.user_id = users.user_id`);
   return result.rows; 
 }
 
-export async function getOne(id) {
-  const result = await pool.query("SELECT * FROM reviews WHERE movie_ID = $1", [id]);
-  return result.rows.length > 0 ? result.rows : null;
-  //return result.rows[0] || null; <- ^ nämä on sama asia eri tavalla kirjoitettuna
+export async function getAllByMovieID(id) {
+  const result = await pool.query( 
+    `SELECT reviews.*, users.username
+     FROM reviews
+     JOIN users ON reviews.user_id = users.user_id
+     WHERE reviews.movie_ID = $1`,
+    [id]);
+  return result.rows; 
+}
+
+export async function getAllByUserID(id) {
+  const result = await pool.query( 
+    `SELECT reviews.*, users.username
+     FROM reviews
+     JOIN users ON reviews.user_id = users.user_id
+     WHERE reviews.user_id = $1`,
+    [id]);
+  return result.rows; 
+}
+
+export async function getOneByReviewID(id) {
+  const result = await pool.query(
+    `SELECT reviews.*, users.username
+     FROM reviews
+     JOIN users ON reviews.user_id = users.user_id
+     WHERE reviews.review_id = $1`,
+    [id]
+  );
+
+  return result.rows.length > 0 ? result.rows[0] : null;
 }
 
 export async function addOne(review) {
---
+++ frontend/src/components/ReviewCard.jsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import "./ReviewCard.css";
+import axios from "axios";
+import { useAuth } from "../contexts/AuthContext.js";
+
+const ReviewCard = ({ review }) => {
+    const [editedReview, setEditedReview] = useState(0);
+    const [editUserStars, setEditUserStars] = useState(1);
+    const [editReviewInput, setEditReviewInput] = useState('');
+    const { user, accessToken } = useAuth();
+
+    function handleSendButton() {
+        axios.put(`${process.env.REACT_APP_API_URL}review/${review.review_id}`,
+            new URLSearchParams({
+                review: editReviewInput,
+                rating: editUserStars
+            }),
+            {
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "Authorization": `Bearer ${accessToken}`
+                },
+                withCredentials: true
+            }
+        ).then(() => {
+            console.log("review updated succesfully!");
+            window.location.reload();
+        }).catch((err) => {
+            console.error('There was an error updating review', err);
+        })
+
+    }
+    function handleEditButton() {
+        setEditedReview(review.review_id);
+    }
+
+    function handleRemoveButton() {
+        axios.delete(`${process.env.REACT_APP_API_URL}review/${review.review_id}`,
+            {
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "Authorization": `Bearer ${accessToken}`
+                },
+                withCredentials: true
+            }
+        ).then(() => {
+            console.log("review deleted succesfully!");
+            window.location.reload();
+        }).catch((err) => {
+            console.error('There was an error deleting review', err);
+        })
+
+    }
+
+    const renderReviewStars = (rating) => {
+        return "★".repeat(rating) + "☆".repeat(5 - rating);
+    };
+
+    return (
+        <div className="review-card">
+            <h4>User: {review.username}</h4>
+            <p className="stars">{renderReviewStars(review.rating)}</p>
+            <p>{review.review}</p>
+            <div className="review-buttons">
+                {user && user.id === review.user_id && (
+                    <>
+                        <button
+                            className="review-edit-button"
+                            onClick={() => handleEditButton()}>
+                            Edit
+                        </button>
+
+                        <button
+                            className="review-edit-button"
+                            onClick={() => handleRemoveButton()}>
+                            Delete
+                        </button>
+                    </>
+                )}
+
+            </div>
+            <div className="editing-screen" style={{ display: editedReview === review.review_id ? "block" : "none" }}>
+                <textarea className="edit-review-input" value={editReviewInput} onChange={e => setEditReviewInput(e.target.value)} />
+                <div className="user-review-star-container">
+                    <button className="edit-review-star" style={{ color: editUserStars >= 1 ? "gold" : "white" }} onClick={() => setEditUserStars(1)}>★</button>
+                    <button className="edit-review-star" style={{ color: editUserStars >= 2 ? "gold" : "white" }} onClick={() => setEditUserStars(2)}>★</button>
+                    <button className="edit-review-star" style={{ color: editUserStars >= 3 ? "gold" : "white" }} onClick={() => setEditUserStars(3)}>★</button>
+                    <button className="edit-review-star" style={{ color: editUserStars >= 4 ? "gold" : "white" }} onClick={() => setEditUserStars(4)}>★</button>
+                    <button className="edit-review-star" style={{ color: editUserStars >= 5 ? "gold" : "white" }} onClick={() => setEditUserStars(5)}>★</button>
+                </div>
+                <button className="send-review-button" onClick={() => handleSendButton(review.review_id)}>Send</button>
+            </div>
+        </div>
+    );
+};
+
+export default ReviewCard;
